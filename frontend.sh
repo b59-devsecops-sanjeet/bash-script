fb9f34904a566903eda3ca5327457737dcb5a11b
sanjeet858 <sanjeetkunwar858@gmail.com>
2025-03-13 23:01:32 +0530

    added backup

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "Installing Nginx"
+echo -e "Installing Nginx"
 dnf install nginx -y &>> /tmp/frontend.log
-echo "Configuring Nginx"
+if [ $? -eq 0 ]; then
+  echo -e "Nginx Installed Successfully"
+else
+  echo -e "Nginx Installation Failed"
+  exit 1
+fi
+echo -e "Configuring Nginx"
 systemctl enable nginx &>> /tmp/frontend.log 
 systemctl start nginx &>> /tmp/frontend.log
