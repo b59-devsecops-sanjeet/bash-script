8400e93fbabd26acc2abe3eae795fe92a21a9178
sanjeet858 <sanjeetkunwar858@gmail.com>
2025-03-15 18:31:32 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,49 +1,34 @@
 #!/bin/bash
 
 component=frontend
-echo -n "Installing Nginx:"
-dnf install nginx -y &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
+logfile=/tmp/frontend.log
+#Common function to print the status of the script
+if [ $1 -eq 0 ]; then
   echo -e "\e[32m Success \e[0m"
 else
   echo -e "Failed"
   exit 1
 fi
+echo -n "Installing Nginx:"
+dnf install nginx -y &>> $logfile
 
 echo -n "Starting Nginx:"
-systemctl enable nginx &>> /tmp/frontend.log 
-systemctl start nginx &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Success \e[0m"
-else
-  echo -e " Failed"
-  exit 1
-fi
+systemctl enable nginx &>> $logfile
+systemctl start nginx &>> $logfile
+stat $?
 
 echo -n "Clearning Old frontend Content:"
 rm -rf /usr/share/nginx/html/* 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Success \e[0m"
-else
-  echo -e "\e[32m Failed \e[0m"
-fi
+stat $?
 
 echo -n "Downloading Frontend Content:"
-curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Success \e[0m"
-else
-  echo -e "\e[32m Failed \e[0m"
-fi
+curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> $logfile
+stat $?
 
 echo -n "Extracting Frontend Content:"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Success \e[0m"
-else
-  echo -e "\e[32m Failed \e[0m"
-fi
+unzip /tmp/$component.zip &>> $logfile
+stat $?
 
 echo -e "Restarting Nginx"
 systemctl restart nginx
