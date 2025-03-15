ec6f9262e6670b868f14b9f53a9ae722ab44cd75
sanjeet858 <sanjeetkunwar858@gmail.com>
2025-03-15 18:17:39 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-echo -e "Installing Nginx"
+
+component=frontend
+echo -n "Installing Nginx:"
 dnf install nginx -y &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "Failed"
+  exit 1
+fi
+
+echo -n "Starting Nginx:"
 systemctl enable nginx &>> /tmp/frontend.log 
 systemctl start nginx &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m Nginx Installed Successfully \e[0m"
+  echo -e "\e[32m Success \e[0m"
 else
-  echo -e "Nginx Installation Failed"
+  echo -e " Failed"
   exit 1
 fi
 
@@ -17,14 +27,16 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[32m Failed \e[0m"
 fi
-echo -e "Downloading Frontend Content"
-curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
+
+echo -n "Downloading Frontend Content:"
+curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Success \e[0m"
 else
   echo -e "\e[32m Failed \e[0m"
 fi
-echo -e "Extracting Frontend Content"
+
+echo -n "Extracting Frontend Content:"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
