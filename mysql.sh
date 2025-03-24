65f21eab2ad458af457aeb83a75ec09c685fec7e
sanjeet858 <sanjeetkunwar858@gmail.com>
2025-03-24 16:44:15 +0530

    mysql cmd

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,10 @@ if [ $(id -u) -ne 0 ]; then
     echo -e "Example usage: \n\t \e[35m sudo bash $0 \e[0m"
     exit 2
 fi
+if[-z $1]; then
+  echo -e "\e[31m Please provide the password \e[0m"
+  exit 1
+fi
 
 stat () {
 if [ $1 -eq 0 ]; then
